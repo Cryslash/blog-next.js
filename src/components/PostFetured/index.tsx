588de71aedb1cf2e47226e678cdc35dce2d99f68
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-06 03:35:58 -0300

    posts queries cache implemented

--- src/components/PostFetured/index.tsx
+++ src/components/PostFetured/index.tsx
@@ -1,10 +1,12 @@
 import { PostCoverImage } from '../PostCoverImage';
-import { PostHeading } from '../PostHeading';
 import { PostSummary } from '../PostSummary';
+import { findAllPublicPosts } from '@/lib/posts/queries';
 
-export function PostFetured() {
-  const slug = 'adsasd';
-  const postLink = `/post/${slug}`;
+export async function PostFetured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+
+  const postLink = `/post/${post.slug}`;
 
   return (
     <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
@@ -15,19 +17,17 @@ export function PostFetured() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: '/images/bryen_9.png',
-          alt: 'Alt da imagem',
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
       <PostSummary
         postHeading='h2'
         postLink={postLink}
-        title={'Dicas para manter a saúde mental em dia'}
-        createdAt={'2025-04-07T00:24:38.616Z'}
-        excerpt={
-          'Em vez de configurar tudo manualmente, basta criar um arquivo com o nome certo e o Next.js entende que aquilo representa uma página.'
-        }
+        title={post.title}
+        createdAt={post.createdAt}
+        excerpt={post.excerpt}
       />
     </section>
   );
