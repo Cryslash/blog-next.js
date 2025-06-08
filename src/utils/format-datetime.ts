d9e812225a3f49a3f98a894903ae1f5bcdc08346
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-08 02:17:21 -0300

    format-datetime utils updated

--- src/utils/format-datetime.ts
+++ src/utils/format-datetime.ts
@@ -4,8 +4,7 @@ import { ptBR } from 'date-fns/locale';
 export function formatDateTime(rawDate: string): string {
   const date = new Date(rawDate);
 
-  // return format(date, "dd/MM/yyyy 'às' HH'h'mm", { locale: ptBR });
-  return format(date, "dd/MM/yyyy HH':'mm", { locale: ptBR });
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", { locale: ptBR });
 }
 
 export function formatRelativeDate(rawDate: string): string {
