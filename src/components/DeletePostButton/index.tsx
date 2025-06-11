b6d0fb5d7e38a99f4120b9f832bbeff85e73731c
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-11 13:33:49 -0300

    deletepostbutton updated

--- src/components/DeletePostButton/index.tsx
+++ src/components/DeletePostButton/index.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { deletePostAction } from '@/app/actions/delete-post-action';
 import clsx from 'clsx';
 import { Trash2Icon } from 'lucide-react';
+import { useState, useTransition } from 'react';
+import { Dialog } from '../Dialog';
 
 type DeletePostButtonProps = {
   title: string;
@@ -9,22 +12,47 @@ type DeletePostButtonProps = {
 };
 
 export function DeletePostButton({ title, id }: DeletePostButtonProps) {
+  const [isPending, startTransition] = useTransition();
+  const [showDialog, setShowDialog] = useState(false);
+
   function handleClick() {
-    alert('Botão Clicado: ' + id);
+    setShowDialog(true);
+  }
+
+  function handleConfirm() {
+    startTransition(async () => {
+      const result = await deletePostAction(id);
+      alert(`O result é ${result}`);
+      setShowDialog(false);
+    });
   }
 
   return (
-    <button
-      className={clsx(
-        'text-red-500 [&_svg]:w-4 [&_svg]:h-4',
-        'cursor-pointer transition',
-        'hover:scale-120 hover:text-red-700',
+    <>
+      <button
+        className={clsx(
+          'text-red-500 [&_svg]:w-4 [&_svg]:h-4',
+          'cursor-pointer transition',
+          'hover:scale-120 hover:text-red-700',
+          'disabled:text-slate-600 disabled:cursor-not-allowed',
+        )}
+        aria-label={`Apagar post: ${title}`}
+        title={`Apagar post: ${title}`}
+        onClick={handleClick}
+        disabled={isPending}
+      >
+        <Trash2Icon />
+      </button>
+      {showDialog && (
+        <Dialog
+          isVisible={showDialog}
+          title='Apagar post?'
+          content={`Deseja apagar o post? ${title}`}
+          onCancel={() => setShowDialog(false)}
+          onConfirm={handleConfirm}
+          disabled={isPending}
+        />
       )}
-      aria-label={`Apagar post: ${title}`}
-      title={`Apagar post: ${title}`}
-      onClick={handleClick}
-    >
-      <Trash2Icon />
-    </button>
+    </>
   );
 }
