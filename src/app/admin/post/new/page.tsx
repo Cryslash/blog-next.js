71ccec969884b09161ce617f13958ace719de705
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-14 19:18:22 -0300

    post/new/page & post/page updated

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from 'next';
 import { ManagePostForm } from '../../../../components/admin/ManagePostForm';
 
 export const dynamic = 'force-dynamic';
 
+export const metadata: Metadata = {
+  title: 'Criar Post',
+};
+
 export default async function AdminPostNewPage() {
-  return <ManagePostForm />;
+  return (
+    <div className='flex flex-col gap-6'>
+      <h1 className='text-xl font-extrabold'>Criar post</h1>
+      <ManagePostForm />;
+    </div>
+  );
 }
