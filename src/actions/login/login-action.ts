543b6abda7796e51e4b8853b7d0914edae4cab2f
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-22 00:26:30 -0300

    loginaction updated

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import { createLoginSession, verifyPassword } from '@/lib/login/manage-login';
 import { AsyncDelay } from '@/utils/async-delay';
+import { verify } from 'crypto';
+import { redirect } from 'next/navigation';
 
 type LoginActionState = {
   username: string;
@@ -10,8 +13,37 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
   await AsyncDelay(5000); //vou manter
 
-  return {
-    username: '',
-    error: 'Teste de erro',
-  };
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados inválidos',
+    };
+  }
+
+  //Dados que o usuário digitou no form
+  const username = formData.get('username')?.toString().trim() || '';
+  const password = formData.get('password')?.toString().trim() || '';
+
+  if (!username || !password) {
+    return {
+      username,
+      error: 'Digite o usuário e a senha',
+    };
+  }
+  //checa se o usuário existe na base de dados
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = await verifyPassword(
+    password,
+    process.env.LOGIN_PASS || '',
+  );
+
+  if (!isUsernameValid && !isPasswordValid) {
+    return {
+      username: '',
+      error: 'Usuário ou senha inválidos',
+    };
+  }
+
+  await createLoginSession(username);
+  redirect('/admin/post');
 }
