6c02094628800d990d75aa04b9b922de5ac97ba4
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-26 02:30:13 -0300

    changeuserpasswordaction created

---
+++ src/actions/users/change-user-password-action.ts
@@ -0,0 +1,77 @@
+'use server';
+
+import { returnCurrentUser, verifyPassword } from '@/lib/login/manage-login';
+import { findUserByName } from '@/lib/users/queries/admin';
+import { userRepository } from '@/repositories/user';
+import { AsyncDelay } from '@/utils/async-delay';
+import { toast } from 'react-toastify';
+
+type ChangeUserPasswordActionState = {
+  success: string;
+  error: string;
+};
+
+export async function ChangeUserPasswordAction(
+  state: ChangeUserPasswordActionState,
+  formData: FormData,
+) {
+  const result = await returnCurrentUser().catch(() => undefined);
+  if (!result) {
+    throw toast.error('Faça login novamente');
+  }
+
+  const userData = await findUserByName(result.username);
+
+  if (!userData) {
+    return {
+      success: '',
+      error: 'Usuário não encontrado na base de dados',
+    };
+  }
+
+  const password = formData.get('password')?.toString().trim() || '';
+  const newPassword = formData.get('newPassword')?.toString().trim() || '';
+  const confirmPassword =
+    formData.get('newPasswordCheck')?.toString().trim() || '';
+
+  const isPasswordValid = await verifyPassword(
+    password,
+    userData.password || '',
+  );
+
+  const isPasswordSame = newPassword === confirmPassword;
+
+  if (!isPasswordValid) {
+    return {
+      success: '',
+      error: 'Senha atual inválida.',
+    };
+  }
+
+  if (!isPasswordSame) {
+    return {
+      success: '',
+      error: 'O campo nova senha e o campo confirmação devem ser iguais',
+    };
+  }
+
+  try {
+    await userRepository.update(userData.name, newPassword);
+  } catch (e) {
+    if (e instanceof Error) {
+      return {
+        success: '',
+        error: e.message,
+      };
+    }
+    return {
+      success: '',
+      error: 'Erro desconhecido',
+    };
+  }
+
+  return {
+    success: 'Senha atualizada com sucesso',
+    error: '',
+  };
+}
