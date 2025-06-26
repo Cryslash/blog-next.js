0d6ceb49c30dd03ad89a659e918c02e325b487ae
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-26 02:36:36 -0300

    userrepository update function

--- src/repositories/user/drizzle-user-repository.ts
+++ src/repositories/user/drizzle-user-repository.ts
@@ -1,6 +1,9 @@
 import { UserModel } from '@/models/user/user-model';
 import { UserRepository } from './user-repository';
 import { drizzleDb } from '@/db/drizzle';
+import { hashPassword, returnCurrentUser } from '@/lib/login/manage-login';
+import { usersTable } from '@/db/drizzle/schemas';
+import { eq } from 'drizzle-orm';
 
 export class DrizzleUserRepository implements UserRepository {
   async findAll(): Promise<UserModel[]> {
@@ -29,5 +32,40 @@ export class DrizzleUserRepository implements UserRepository {
     if (!user) throw new Error('Nome de usuario não encontrado');
 
     return user;
+  }
+
+  async update(userName: string, newPassword: string): Promise<UserModel> {
+    const user = await drizzleDb.query.users.findFirst({
+      where: (user, { eq }) => eq(user.name, userName),
+    });
+
+    if (!user) {
+      throw new Error('Usuário não encontrado na base de dados');
+    }
+
+    const result = await returnCurrentUser().catch(() => undefined);
+    if (!result) {
+      throw new Error('Faça login novamente');
+    }
+
+    if (result.username != userName) {
+      throw new Error(
+        'Ação bloqueada! Se você acha que isso é um erro contate um administrador... ',
+      );
+    }
+
+    const hashedPassword = await hashPassword(newPassword);
+
+    const newUserData = { password: hashedPassword };
+
+    await drizzleDb
+      .update(usersTable)
+      .set(newUserData)
+      .where(eq(usersTable.name, userName));
+
+    return {
+      ...user,
+      ...newUserData,
+    };
   }
 }
--- src/repositories/user/user-repository.ts
+++ src/repositories/user/user-repository.ts
@@ -4,4 +4,7 @@ export interface UserRepository {
   findAll(): Promise<UserModel[]>;
   findById(id: number): Promise<UserModel>;
   findByName(name: string): Promise<UserModel>;
+
+  //mutations
+  update(userName: string, newPassword: string): Promise<UserModel>;
 }
