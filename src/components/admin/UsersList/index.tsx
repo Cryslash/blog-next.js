9ddf150a17b47c1687e05069dc5b8b64a9946f3a
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-28 19:54:22 -0300

    userlist component updated

--- src/components/admin/UsersList/index.tsx
+++ src/components/admin/UsersList/index.tsx
@@ -1,36 +1,24 @@
-import { Button } from '@/components/Button';
 import { findAllUsers } from '@/lib/users/queries/admin';
-import clsx from 'clsx';
-import { RefreshCcwIcon, TrashIcon } from 'lucide-react';
 import { DeleteUserButton } from '../DeleteUserButton';
+import { ChangePasswordAdminButton } from '../ChangePasswordAdminButton';
 
 export async function UsersList() {
   const users = await findAllUsers();
 
   return (
     <div className='flex flex-col mt-8 mb-16 mx-auto gap-y-8 sm:mb-32'>
-      <h1 className='text-xl text-center'>Gerenciar usuários</h1>
+      <h3 className='text-xl text-center'>Gerenciar usuários</h3>
       {users.map(user => {
         return (
           <div
             key={user.id}
-            className='flex justify-between items-center px-2 gap-10 bg-slate-200 '
+            className='flex justify-between items-center w-72 px-3 bg-slate-200 sm:w-90'
           >
             <p>{user.name}</p>
             <p>{user.userType}</p>
-            <div className='flex flex-row'>
-              <Button
-                className={clsx(
-                  'text-blue-800 [&_svg]:w-4 [&_svg]:h-4 bg-transparent',
-                  'cursor-pointer transition',
-                  'hover:scale-120 hover:text-blue-900 hover:bg-transparent',
-                  'disabled:text-slate-600 disabled:cursor-not-allowed',
-                )}
-                title='Mudar senha'
-                aria-label='Mudar senha'
-              >
-                <RefreshCcwIcon />
-              </Button>
+            <div className='flex flex-row gap-5 h-9'>
+              <ChangePasswordAdminButton username={user.name} id={user.id} />
+
               <DeleteUserButton username={user.name} id={user.id} />
             </div>
           </div>
