a9a27dea7d9746647ae497abaa6b19eee73f76fd
Crystofher Lins <crystofher_lins@outlook.com>
2025-06-30 18:56:13 -0300

    restoreuseraction created

---
+++ src/actions/users/restore-user-action.ts
@@ -0,0 +1,57 @@
+'use server';
+
+import { drizzleDb } from '@/db/drizzle';
+import { usersTable } from '@/db/drizzle/schemas';
+import { returnCurrentUser } from '@/lib/login/manage-login';
+import { findUserById } from '@/lib/users/queries/admin';
+import { eq } from 'drizzle-orm';
+import { revalidateTag } from 'next/cache';
+
+export async function RestoreUserAction(id?: number) {
+  if (!id) {
+    return { error: 'Usuário inválido' };
+  }
+
+  const result = await returnCurrentUser().catch(() => undefined);
+  if (!result) {
+    return {
+      error: 'Faça login novamente',
+    };
+  }
+
+  const { usertype } = result;
+  if (usertype != 'admin') {
+    return {
+      error: 'Você não tem autorização para realizar a ação',
+    };
+  }
+
+  const user = await findUserById(id);
+  if (!user) {
+    return {
+      error: 'Usuário não encontrado na base de dados',
+    };
+  }
+
+  const newUserData = { isActive: true };
+
+  try {
+    await drizzleDb
+      .update(usersTable)
+      .set(newUserData)
+      .where(eq(usersTable.id, id));
+  } catch (e) {
+    if (e instanceof Error) {
+      return {
+        error: e.message,
+      };
+    }
+    return {
+      error: 'Erro desconhecido',
+    };
+  }
+
+  revalidateTag('users');
+
+  return { error: '' };
+}
